9e772dea3b0fc0324cace3f4500caf1d09e2a2ec
Lakshit Singh <lakshitsingh0228@gmail.com>
2022-04-26 13:55:58 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,22 @@ class MyApp extends StatelessWidget {
     
     //Num will take int as well as double type datatypes
     return MaterialApp(
-      home: HomePage(),
+     
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context)=>LoginPage(),
+        "/home" : (context)=>HomePage(),
+        "/login" : (context)=>LoginPage()
+        
+      },
     );
   }
 }
+ 
