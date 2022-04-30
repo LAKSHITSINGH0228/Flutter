e4725fb664cc7a451f466950a8928e1978dc3a42
Lakshit Singh <lakshitsingh0228@gmail.com>
2022-04-30 16:57:26 +0530

    Day 11 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+  Item(id: 1,
+   name: "iPhone 12 pro max",
+   desc: "Apple iPhone 12th generation",
+   price: 999,
+   color: "#33505a",
+   image: "assets/images/iPhone12ProMax.jpg"),
+];
+}
+
+
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -9,13 +22,3 @@ class Item {
   Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
 }
 
-final products = [
-  Item(id: "Codepur001",
-   name: "iPhone 12 pro max",
-   desc: "Apple iPhone 12th generation",
-   price: 999,
-   color: "#33505a",
-   image: "assets/images/iphone12ProMax.jpg"),
-
-
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 
-class  HomePage extends StatelessWidget {
+import '../widgets/item_widget.dart';
+
+class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Flutter";
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
-        title:const Text("Catalog App"),
+        title: const Text("Catalog App"),
       ),
-        body: Center(
-          child: Container( 
-            child: Text(context.runtimeType.toString()),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount:dummyList.length,
+          //itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              //item: CatalogModel.items[index],
+              item: dummyList[index]
+            );
+          },
         ),
-        drawer: MyDrawer(),
-      );
+      ),
+      drawer: MyDrawer(),
+    );
   }
-} 
+}
