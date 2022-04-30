d1076e77dabf0b0f1efa3160917c69f84d62e06b
Lakshit Singh <lakshitsingh0228@gmail.com>
2022-04-30 23:54:06 +0530

    Day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_null_comparison, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
@@ -24,35 +26,35 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    // ignore: unused_local_variable
+    await Future.delayed(Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
     final decodedData = jsonDecode(catalogJson);
     final productsData = decodedData["products"];
-    print(productsData);
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          //itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-                //item: CatalogModel.items[index],
-                item: dummyList[index]);
-          },
+        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) => ItemWidget(
+                item: CatalogModel.items[index]),
+        ): Center(child: 
+        CircularProgressIndicator(),
         ),
       ),
       drawer: MyDrawer(),
-    );
+      );
   }
 }
